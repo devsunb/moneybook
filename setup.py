1f6d091818632ca0ebc75f7c18b2acc59cbd350d
Jaeseok Lee <jaeseoklee00@gmail.com>
2020-01-09 14:28:36 +0900

    Initial commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+with open('VERSION_JENKINS', mode='r') as f:
+    VERSION = f.readline().strip()
+
+setup(
+    name='moneybook',
+    version=VERSION,
+    description='편한가계부 PC가계부 API 도구',
+    author='Jaeseok Lee',
+    author_email='jaeseoklee00@gmail.com',
+    packages=find_packages(exclude=['test']),
+    setup_requires=[],
+    zip_safe=False,
+    install_requires=['argparse', 'openpyxl']
+)
